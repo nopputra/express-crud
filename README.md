ef390c5f4f170d7e8d45bb5a8d25aecd32c40b03
NopPutra <nopputra007@gmail.com>
2026-05-14 14:36:11 +0700

    First Init Project

--- README.md
+++
@@ -1,5 +0,0 @@
-**Create Project**
-
-> npm init  // _install package.json_
-
-> npm install express  // _install modules_
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const PORT = 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "* "}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
